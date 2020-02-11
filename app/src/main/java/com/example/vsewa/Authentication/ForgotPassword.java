3b985100996598585c30b26e30ad8221e4de4dfa
Asif <asifanwar007>
2020-02-11 23:49:38 +0530

    login activity added

---
+++ app/src/main/java/com/example/vsewa/Authentication/ForgotPassword.java
@@ -0,0 +1,16 @@
+package com.example.vsewa.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vsewa.R;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/example/vsewa/Authentication/RegisterWithEmailActivity.java
@@ -0,0 +1,16 @@
+package com.example.vsewa.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vsewa.R;
+
+public class RegisterWithEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_with_email);
+    }
+}
