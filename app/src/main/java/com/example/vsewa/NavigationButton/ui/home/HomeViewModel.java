8d4d238907e48a9687a7a8ae1d6e57a2bfa4f994
Asif <asifanwar0000@outlook.com>
2020-04-05 22:52:30 +0530

    Model provide has some errors

--- app/src/main/java/com/example/vsewa/NavigationButton/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/vsewa/NavigationButton/ui/home/HomeViewModel.java
@@ -1,7 +1,12 @@
 package com.example.vsewa.NavigationButton.ui.home;
 
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.widget.Switch;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -12,13 +17,17 @@ import com.google.firebase.auth.FirebaseUser;
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
-    private Switch volunteerSwitch;
     private FirebaseUser mUser;
 
-    public HomeViewModel() {
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+
+    HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("Welcome");
         mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //location check and update
 
     }
 
