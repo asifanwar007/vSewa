a36fefd0ab586a599886312a8abfa44bdfb601c3
abslan <poojith2448@gmail.com>
2020-08-26 19:56:19 +0530

    messenger added

---
+++ app/src/main/java/com/example/vsewa/Messenger/MessengerActivity.java
@@ -0,0 +1,56 @@
+package com.example.vsewa.Messenger;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vsewa.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+
+public class MessengerActivity extends AppCompatActivity {
+    private DatabaseReference myDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDatabase = FirebaseDatabase.getInstance().getReference("Message");
+
+        final TextView myText = findViewById(R.id.textbox);
+        myDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                myText.setText(Objects.requireNonNull(dataSnapshot.getValue()).toString());
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                myText.setText("CANCELLED");
+
+            }
+        });
+    }
+
+    public void sendMessage(View view){
+        EditText myEditText = findViewById(R.id.editText);
+
+        myDatabase.push().setValue(myEditText.getText().toString());
+        myEditText.setText("");
+    }
+}
